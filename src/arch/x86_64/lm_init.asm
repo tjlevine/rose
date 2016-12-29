ea1bd25e671959542b0f093be4bc49cd8d24e193
Tyler Levine <tyler@tylerlevine.com>
2016-12-29 12:12:10 -0800

    Add rust package and entrypoint, print to the screen from Rust-land
    * Add call to rust code from long mode init after setup
    * Note: we have exhausted our small 64 byte stack at this
      point, so in order to do more in rust we will need to expand the
      program's available stack space.

--- src/arch/x86_64/lm_init.asm
+++ src/arch/x86_64/lm_init.asm
@@ -9,7 +9,16 @@ VGA_BUFFER_ADDR equ 0xB8000    ; physical address of the VGA text buffer
 section .text
 bits 64
 lm_start:
-    ; print OKAY to screen
-    mov rax, OKAY
-    mov qword [VGA_BUFFER_ADDR], rax
+    ; call the rust entry point
+    extern rust_main
+    call rust_main
+
+.os_returned:
+    ; rust main returned, print `OS returned!`
+    mov rax, 0x4F724F204F534F4F
+    mov [VGA_BUFFER_ADDR + 0x00], rax
+    mov rax, 0x4F724F754F744F65
+    mov [VGA_BUFFER_ADDR + 0x08], rax
+    mov rax, 0x4F214F644F654F6E
+    mov [VGA_BUFFER_ADDR + 0x10], rax
     hlt
